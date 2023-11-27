3e36849d65eb9d4fd77e9a03bf0874b02e754eba
Badru <badruakfm@gmail.com>
2023-11-27 18:37:28 +0700

    FIX(Cart: stock) - Fixes issue with item stock doesn't change when item add/minus to or in cart - Fixes issue with item stock still change when item stock is 0

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -2,13 +2,30 @@ const { cart: cartModel, item: itemModel } = require('../../models')
 
 exports.addToCart = async (req, res, next) => {
     try {
+        const { item_id } = req.body
+
+        const item = await itemModel.findByPk(item_id)
+        if (item.stock == 0) {
+            const error = new Error()
+            error.errorCode = 400
+            error.message = 'Maaf, stok untuk item ini sedang kosong. Silakan coba pesan kembali nanti.'
+            throw error
+        }
         const result = await cartModel.findOrCreate({
             where: {
                 user_id: req.userId,
-                item_id: req.body.item_id
+                item_id,
             },
             defaults: {
                 qty: 1
+            }
+        })
+
+        await itemModel.increment({
+            stock: -1
+        }, {
+            where: {
+                id: item_id
             }
         })
 
@@ -43,11 +60,30 @@ exports.getCartData = async (req, res, next) => {
 
 exports.plusItemInCart = async (req, res, next) => {
     try {
+        const { id } = req.params
+
+        const cartItem = await cartModel.findByPk(id)
+        const item = await itemModel.findByPk(cartItem.item_id)
+        if (item.stock == 0) {
+            const error = new Error()
+            error.errorCode = 400
+            error.message = 'Maaf, stok untuk item ini sedang kosong. Silakan coba pesan kembali nanti.'
+            throw error
+        }
         const result = await cartModel.increment({
             qty: 1
         }, {
             where: {
-                id: req.params.id
+                id
+            }
+        })
+
+
+        await itemModel.increment({
+            stock: -1
+        }, {
+            where: {
+                id: cartItem.item_id
             }
         })
 
@@ -72,6 +108,15 @@ exports.minusItemInCart = async (req, res, next) => {
                     id
                 }
             })
+
+            const cartItem = await cartModel.findByPk(id)
+            await itemModel.increment({
+                stock: 1
+            }, {
+                where: {
+                    id: cartItem.item_id
+                }
+            })
         }
 
         res.status(200).send({
@@ -84,6 +129,25 @@ exports.minusItemInCart = async (req, res, next) => {
 
 exports.clearItemInCart = async (req, res, next) => {
     try {
+        const cartData = await cartModel.findAll({
+            where: {
+                user_id: req.userId,
+            },
+        })
+
+        const minusStock = async (itemId, stock) => {
+            await itemModel.increment({
+                stock: stock
+            }, {
+                where: {
+                    id: itemId
+                }
+            })
+        }
+
+        const minusStocks = cartData.map(data => minusStock(data.dataValues.item_id, data.dataValues.qty))
+        await Promise.all(minusStocks)
+
         const cart = await cartModel.destroy({
             where: {
                 user_id: req.userId,
